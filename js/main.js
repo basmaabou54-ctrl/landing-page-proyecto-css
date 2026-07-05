bd42038fc18fd387e1e29111c9a9348c1025a5f8
basmaabou54-ctrl <basmaabou54@gmail.com>
2026-07-05 20:37:00 +0200

    section contacto con formData

---
+++ js/main.js
@@ -0,0 +1,63 @@
+
+const formulario = document.querySelector('#formContacto');
+const inputNombre = document.querySelector('#nombre');
+const inputEmail = document.querySelector('#email');
+const inputMensaje = document.querySelector('#mensaje');
+const mensajeExito = document.querySelector('#mensajeExito');
+
+const todosLosInputs = [inputNombre, inputEmail, inputMensaje];
+
+
+formulario.addEventListener('submit', (event) => {
+  
+    event.preventDefault();
+
+
+    const formData = new FormData(event.target);
+    const datos = Object.fromEntries(formData);
+
+    let hayError = false;
+
+  
+    const mostrarError = (input) => {
+        input.classList.add('error'); // Border rojo
+        input.nextElementSibling.classList.add('visible'); 
+        hayError = true;
+    };
+
+    
+    
+   
+    if (datos.nombre.trim() === '') {
+        mostrarError(inputNombre);
+    }
+
+  
+    if (datos.email.trim() === '' || !datos.email.includes('@')) {
+        mostrarError(inputEmail);
+    }
+
+ 
+    if (datos.mensaje.trim() === '') {
+        mostrarError(inputMensaje);
+    }
+
+   
+    if (!hayError) {
+        mensajeExito.classList.add('visible'); // mendaje de éxito
+        formulario.reset(); 
+    }
+});
+
+//quittar error en tiempo real
+todosLosInputs.forEach(input => {
+    input.addEventListener('input', () => {
+        if (input.value.trim() !== '') {
+            input.classList.remove('error');
+            input.nextElementSibling.classList.remove('visible');
+            
+        //quittar mensaje de éxito si el usuario empieza a escribir de nuevo
+            mensajeExito.classList.remove('visible');
+        }
+    });
+});
