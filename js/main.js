a33a9af8cea6fbf8816e4af3838580a881775b85
basmaabou54-ctrl <basmaabou54@gmail.com>
2026-07-08 11:31:13 +0200

    mejorar mostrar y ocultar banner

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const todosLosInputs = [inputNombre, inputEmail, inputMensaje];
 
 
 formulario.addEventListener('submit', (event) => {
-  
+
     event.preventDefault();
 
 
@@ -19,34 +19,34 @@ formulario.addEventListener('submit', (event) => {
 
     let hayError = false;
 
-  
+
     const mostrarError = (input) => {
         input.classList.add('error'); // Border rojo
-        input.nextElementSibling.classList.add('visible'); 
+        input.nextElementSibling.classList.add('visible');
         hayError = true;
     };
 
-    
-    
-   
+
+
+
     if (datos.nombre.trim() === '') {
         mostrarError(inputNombre);
     }
 
-  
+
     if (datos.email.trim() === '' || !datos.email.includes('@')) {
         mostrarError(inputEmail);
     }
 
- 
+
     if (datos.mensaje.trim() === '') {
         mostrarError(inputMensaje);
     }
 
-   
+
     if (!hayError) {
         mensajeExito.classList.add('visible'); // mendaje de éxito
-        formulario.reset(); 
+        formulario.reset();
     }
 });
 
@@ -56,8 +56,8 @@ todosLosInputs.forEach(input => {
         if (input.value.trim() !== '') {
             input.classList.remove('error');
             input.nextElementSibling.classList.remove('visible');
-            
-        //quittar mensaje de éxito si el usuario empieza a escribir de nuevo
+
+            //quittar mensaje de éxito si el usuario empieza a escribir de nuevo
             mensajeExito.classList.remove('visible');
         }
     });
@@ -69,6 +69,7 @@ bannieres.forEach(banniere => {
     banniere.addEventListener('click', () => {
         const galerie = banniere.nextElementSibling;
         galerie.classList.toggle('activo');
+        banniere.classList.toggle('activo');
     });
 });
 
